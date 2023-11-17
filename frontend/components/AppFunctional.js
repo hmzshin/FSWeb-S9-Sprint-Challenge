5ffa064d0d294a114bb835fb266991f66914d2d8
hmzshin <hamzasahin@outlook.com>
2023-11-17 18:47:37 +0300

    set position of the b accoridng to coordinates

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -13,6 +13,7 @@ export default function AppFunctional(props) {
   const [steps, setSteps] = useState(initialSteps);
   const [coordinate, setCoordinate] = useState(initiaCoordinate);
   const [direction, setDirection] = useState([]);
+  const [index, setIndex] = useState(4);
 
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
@@ -57,24 +58,39 @@ export default function AppFunctional(props) {
         : setCoordinate(coordinate);
     }
 
-    showCoordinates(coordinate);
-    console.log(direction);
+    console.log("direction:", direction);
+    console.log("coordinate: ", coordinate);
   }, [direction]);
 
   // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
   // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
   // şu anki indeksi değiştirmemeli.
 
-  function changeHandler(e) {
-    const { value, name } = e.target;
+  useEffect(() => {
+    if (coordinate[0] == 1 && coordinate[1] == 1) {
+      console.log("arrangeIndex fonksiyonu çalıştı");
+      setIndex(0);
+    } else if (coordinate[0] == 1 && coordinate[1] == 2) {
+      setIndex(1);
+    } else if (coordinate[0] == 1 && coordinate[1] == 3) {
+      setIndex(2);
+    } else if (coordinate[0] == 2 && coordinate[1] == 1) {
+      setIndex(3);
+    } else if (coordinate[0] == 2 && coordinate[1] == 2) {
+      setIndex(4);
+    } else if (coordinate[0] == 2 && coordinate[1] == 3) {
+      setIndex(5);
+    } else if (coordinate[0] == 3 && coordinate[1] == 1) {
+      setIndex(6);
+    } else if (coordinate[0] == 3 && coordinate[1] == 3) {
+      setIndex(7);
+    } else if (coordinate[0] == 3 && coordinate[1] == 3) {
+      setIndex(8);
+    }
 
     // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
     // ve buna göre state i değiştirir.
-  }
-
-  function onChange(evt) {
-    // inputun değerini güncellemek için bunu kullanabilirsiniz
-  }
+  }, [coordinate]);
 
   function submitHandler(e) {
     e.preventDefault();
@@ -92,8 +108,8 @@ export default function AppFunctional(props) {
       </div>
       <div id="grid">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
-          <div key={idx} className={`square${idx === 4 ? " active" : ""}`}>
-            {idx === 4 ? "B" : null}
+          <div key={idx} className={`square${idx === index ? " active" : ""}`}>
+            {idx === index ? "B" : null}
           </div>
         ))}
       </div>
